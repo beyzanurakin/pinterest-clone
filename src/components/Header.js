fbd17f9b98a8c0b9e137935582b01a86625587a5
beyzanurakin <akinbeyzanur@gmail.com>
2022-07-04 22:42:07 +0300

    Design Header component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,11 @@
 import React from 'react'
 import PinterestIcon from '@mui/icons-material/Pinterest'
 import IconButton from '@mui/material/IconButton'
+import SearchIcon from '@mui/icons-material/Search'
+import NotificationsIcon from '@mui/icons-material/Notifications'
+import TextsmsIcon from '@mui/icons-material/Textsms'
+import FaceIcon from '@mui/icons-material/Face'
+import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown'
 import styled from 'styled-components'
 
 function Header() {
@@ -19,9 +24,31 @@ function Header() {
           <a href='/'>Following</a>
         </FollowingButton>
         <SearchWrapper>
-          <SearhBarWrapper></SearhBarWrapper>
+          <SearchBarWrapper>
+            <IconButton>
+              <SearchIcon />
+            </IconButton>
+            <form>
+              <input type='text' />
+              {/* to seacrh by enter */}
+              <button type='submit'></button>
+            </form>
+          </SearchBarWrapper>
         </SearchWrapper>
-        <IconsWrapper></IconsWrapper>
+        <IconsWrapper>
+          <IconButton>
+            <NotificationsIcon />
+          </IconButton>
+          <IconButton>
+            <TextsmsIcon />
+          </IconButton>
+          <IconButton>
+            <FaceIcon />
+          </IconButton>
+          <IconButton>
+            <KeyboardArrowDownIcon />
+          </IconButton>
+        </IconsWrapper>
       </Wrapper>
     </div>
   )
@@ -44,10 +71,74 @@ const LogoWrapper = styled.div`
     cursor: pointer;
   }
 `
-const HomePageButton = styled.div`
+const HomeButtons = styled.div`
   display: flex;
   height: 48px;
   min-width: 123px;
   align-items: center;
   justify-content: center;
+  border-radius: 24px;
+  cursor: pointer;
 `
+
+const HomePageButton = styled(HomeButtons)`
+  background-color: rgb(17, 17, 17);
+
+  a {
+    text-decoration: none;
+    color: white;
+    font-weight: 700;
+  }
+`
+const FollowingButton = styled(HomeButtons)`
+  background-color: white;
+
+  a {
+    text-decoration: none;
+    color: rgb(17, 17, 17);
+    font-weight: 700;
+  }
+  :hover {
+    background-color: #e1e1e1;
+  }
+`
+
+const SearchWrapper = styled.div`
+  flex: 1;
+`
+const SearchBarWrapper = styled.div`
+  background-color: #efefef;
+  display: flex;
+  height: 48px;
+  width: 100%;
+  border-radius: 50px;
+  border: none;
+  padding-left: 10px;
+
+  form {
+    display: flex;
+    flex: 1;
+  }
+
+  form > input {
+    background-color: transparent;
+    border: none;
+    width: 100%;
+    margin-left: 5px;
+    font-size: 16px;
+  }
+  form > button {
+    display: none;
+  }
+
+  input:focus {
+    outline: none;
+  }
+  input:active {
+    outline: none;
+  }
+  input:hover {
+    outline: none;
+  }
+`
+const IconsWrapper = styled.div``
